e6b19b23aa82c5844a62dcb57235d7a1851172bb
Markkus Hipponen <markkus.hipponen@khk.ee>
2021-08-02 15:22:26 +0300

    Koostatud ja testitud päringu ettevalmistus, väärtustega täitmine ja käivitamine

--- app/libs/Database.php
+++ app/libs/Database.php
@@ -10,6 +10,7 @@ class Database
 
     private $dbh;
     private $error;
+    private $stmt;
 
     /**
      * Database constructor.
@@ -28,6 +29,33 @@ class Database
             $this->error = $e->getMessage();
             echo $this->error.'<br>';
         }
+    }
+
+    public function query($sql){
+        $this->stmt = $this->dbh->prepare($sql);
+    }
+
+    public function bind($param, $value, $type=null){
+        if(is_null($type)){
+            switch (true){
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->stmt->bindParam($param, $value, $type);
+    }
+
+    public function execute(){
+        $this->stmt->execute();
     }
 
 }
