37801f672f1ff3d48301f788717d3678c0dbff98
Markkus Hipponen <markkus.hipponen@khk.ee>
2021-08-02 13:28:40 +0300

    Loodud põhikontroller ja määratud vaade faili laadimisvõimalused

---
+++ app/libs/Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class Controller
+{
+    // load view
+    public function view($view, $data = array()){
+        if(file_exists('../app/views'.$view.'.php')){
+            require_once '../app/views'.$view.'.php';
+        } else {
+            die('View '.$view.' does not exists');
+        }
+    }
+}
