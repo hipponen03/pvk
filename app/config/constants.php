e94c65024e8c0af557db592cb6ef406d715573d3
Markkus Hipponen <markkus.hipponen@khk.ee>
2021-08-02 16:59:45 +0300

    Rakenduse põhi - raamistik valmis

--- app/config/constants.php
+++ app/config/constants.php
@@ -1,13 +1,13 @@
 <?php
 // Project name
-define('PROJECT_NAME', 'pvk');
+define('PROJECT_NAME', 'projectname');
 // App root dir
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL root
 define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
 
 // db params
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'pvk');
+define('DB_HOST', 'host');
+define('DB_USER', 'user');
+define('DB_PASS', 'pass');
+define('DB_NAME', 'dbname');
--- app/models/Page.php
+++ app/models/Page.php
@@ -5,36 +5,8 @@ class Page
 {
     private $db;
 
-    /**
-     * Page constructor.
-     */
     public function __construct()
     {
         $this->db = new Database();
-    }
-
-    public function addUser(){
-        $this->db->query('INSERT INTO users SET name=:name, email=:email, status=:status');
-        $this->db->bind('name', 'user2');
-        $this->db->bind('email', 'user2@host.loc');
-        $this->db->bind('status', 'admin');
-        $this->db->execute();
-    }
-
-
-    public function getOneUser(){
-        $this->db->query('SELECT * FROM users WHERE id=:id');
-        $this->db->bind('id', 1);
-        return $this->db->getOne();
-    }
-
-    public function getUsers(){
-        $this->db->query('SELECT * FROM users');
-        return $this->db->getALl();
-    }
-
-    public function usersCount(){
-        $this->getUsers();
-        return $this->db->rowCount();
     }
 }
